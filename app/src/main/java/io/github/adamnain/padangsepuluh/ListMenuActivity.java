8f54d0fbcce980dbe9019882bc97ba2af422ac5c
adamnain <adamdzulqarnain@gmail.com>
2018-10-09 14:36:40 +0700

    add menu and context menu

--- app/src/main/java/io/github/adamnain/padangsepuluh/ListMenuActivity.java
+++ app/src/main/java/io/github/adamnain/padangsepuluh/ListMenuActivity.java
@@ -3,6 +3,11 @@ package io.github.adamnain.padangsepuluh;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +29,26 @@ public class ListMenuActivity extends AppCompatActivity {
     TextView tvJumTeh;
     @BindView(R.id.tv_totalHarga)
     TextView tvTotalHarga;
+    @BindView(R.id.btn_plusNasi)
+    Button btnPlusNasi;
+    @BindView(R.id.btn_minNasi)
+    Button btnMinNasi;
+    @BindView(R.id.btn_plusRendang)
+    Button btnPlusRendang;
+    @BindView(R.id.btn_minRendang)
+    Button btnMinRendang;
+    @BindView(R.id.btn_plusAyam)
+    Button btnPlusAyam;
+    @BindView(R.id.btn_minAyam)
+    Button btnMinAyam;
+    @BindView(R.id.btn_plusIkan)
+    Button btnPlusIkan;
+    @BindView(R.id.btn_minIkan)
+    Button btnMinIkan;
+    @BindView(R.id.btn_plusTeh)
+    Button btnPlusTeh;
+    @BindView(R.id.btn_minTeh)
+    Button btnMinTeh;
 
     int totalHarga, jumNasi, jumRendang, jumAyam, jumIkan, jumTeh;
 
@@ -39,7 +64,126 @@ public class ListMenuActivity extends AppCompatActivity {
         jumAyam = 0;
         jumIkan = 0;
         jumTeh = 0;
+
+        backButton();
+        //register contex menu
+        registerForContextMenu(btnPlusNasi);
+        registerForContextMenu(btnMinNasi);
+        registerForContextMenu(btnPlusRendang);
+        registerForContextMenu(btnMinRendang);
+        registerForContextMenu(btnPlusAyam);
+        registerForContextMenu(btnMinAyam);
+        registerForContextMenu(btnPlusIkan);
+        registerForContextMenu(btnMinIkan);
+        registerForContextMenu(btnPlusTeh);
+        registerForContextMenu(btnMinTeh);
     }
+
+    //override context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        //menu.setHeaderTitle("Context Menu");
+        menu.add(0, v.getId(), 0, "1");
+        menu.add(0, v.getId(), 0, "2");
+        menu.add(0, v.getId(), 0, "3");
+        menu.add(0, v.getId(), 0, "4");
+        menu.add(0, v.getId(), 0, "5");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int angkaContexMenu = Integer.valueOf(item.getTitle().toString());
+        item.getItemId();
+        if (item.getItemId() == R.id.btn_plusNasi){
+            int harga = 3000*angkaContexMenu;
+            jumNasi = jumNasi+angkaContexMenu;
+            tvJumNasi.setText(""+jumNasi);
+            setTextTotalHarga(totalHarga(harga));
+        }
+        else if ((item.getItemId() == R.id.btn_minNasi)){
+            if(Integer.valueOf(tvJumNasi.getText().toString())-angkaContexMenu < 0){
+                Toast.makeText(this, "Pesanan Tidak dapat Negatif", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                int harga = -3000*angkaContexMenu;
+                jumNasi = jumNasi-angkaContexMenu;
+                tvJumNasi.setText(""+jumNasi);
+                setTextTotalHarga(totalHarga(harga));
+            }
+
+        }
+        else if (item.getItemId() == R.id.btn_plusRendang){
+            int harga = 7000*angkaContexMenu;
+            jumRendang = jumRendang+angkaContexMenu;
+            tvJumRendang.setText(""+jumRendang);
+            setTextTotalHarga(totalHarga(harga));
+        }
+        else if ((item.getItemId() == R.id.btn_minRendang)){
+            if(Integer.valueOf(tvJumRendang.getText().toString())-angkaContexMenu < 0){
+                Toast.makeText(this, "Pesanan Tidak dapat Negatif", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                int harga = -7000*angkaContexMenu;
+                jumRendang = jumRendang-angkaContexMenu;
+                tvJumRendang.setText(""+jumRendang);
+                setTextTotalHarga(totalHarga(harga));
+            }
+        }
+        else if (item.getItemId() == R.id.btn_plusAyam){
+            int harga = 7000*angkaContexMenu;
+            jumAyam = jumAyam+angkaContexMenu;
+            tvJumAyam.setText(""+jumAyam);
+            setTextTotalHarga(totalHarga(harga));
+        }
+        else if ((item.getItemId() == R.id.btn_minAyam)){
+            if(Integer.valueOf(tvJumAyam.getText().toString())-angkaContexMenu < 0){
+                Toast.makeText(this, "Pesanan Tidak dapat Negatif", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                int harga = -7000*angkaContexMenu;
+                jumAyam = jumAyam-angkaContexMenu;
+                tvJumAyam.setText(""+jumAyam);
+                setTextTotalHarga(totalHarga(harga));
+            }
+        }
+        else if (item.getItemId() == R.id.btn_plusIkan){
+            int harga = 7000*angkaContexMenu;
+            jumIkan = jumIkan+angkaContexMenu;
+            tvJumIkan.setText(""+jumIkan);
+            setTextTotalHarga(totalHarga(harga));
+        }
+        else if ((item.getItemId() == R.id.btn_minIkan)){
+            if(Integer.valueOf(tvJumIkan.getText().toString())-angkaContexMenu < 0){
+                Toast.makeText(this, "Pesanan Tidak dapat Negatif", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                int harga = -7000*angkaContexMenu;
+                jumIkan = jumIkan-angkaContexMenu;
+                tvJumIkan.setText(""+jumIkan);
+                setTextTotalHarga(totalHarga(harga));
+            }
+        }
+        else if (item.getItemId() == R.id.btn_plusTeh){
+            int harga = 4000*angkaContexMenu;
+            jumTeh = jumTeh+angkaContexMenu;
+            tvJumTeh.setText(""+jumTeh);
+            setTextTotalHarga(totalHarga(harga));
+        }
+        else if ((item.getItemId() == R.id.btn_minTeh)){
+            if(Integer.valueOf(tvJumTeh.getText().toString())-angkaContexMenu < 0){
+                Toast.makeText(this, "Pesanan Tidak dapat Negatif", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                int harga = -4000*angkaContexMenu;
+                jumTeh = jumTeh-angkaContexMenu;
+                tvJumTeh.setText(""+jumTeh);
+                setTextTotalHarga(totalHarga(harga));
+            }
+        }
+        return true;
+    }
+
 
     private int totalHarga(int harga){
         totalHarga = totalHarga+harga;
@@ -49,6 +193,7 @@ public class ListMenuActivity extends AppCompatActivity {
     private void setTextTotalHarga(int harga){
         tvTotalHarga.setText(""+harga);
     }
+
 
     @OnClick(R.id.btn_plusNasi)
     public void plusNasi(){
@@ -165,5 +310,20 @@ public class ListMenuActivity extends AppCompatActivity {
         Intent i = new Intent(getApplicationContext(), PesanActivity.class);
         i.putExtra("totalHarga", tvTotalHarga.getText().toString());
         startActivity(i);
+    }
+
+    //untuk enampilkan back button
+    public void backButton(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Daftar Menu");
+    }
+
+    //fungsi back ketika tombol back diklik
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/io/github/adamnain/padangsepuluh/MainActivity.java
+++ app/src/main/java/io/github/adamnain/padangsepuluh/MainActivity.java
@@ -7,6 +7,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.viewpagerindicator.CirclePageIndicator;
 
@@ -33,6 +37,31 @@ public class MainActivity extends AppCompatActivity {
         initSlider();
         ButterKnife.bind(this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_menu:
+                listMenu();
+                return true;
+            case R.id.action_call:
+                callRumahMakan();
+                return true;
+            case R.id.action_map:
+                lokasiMenu();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void initSlider() {
